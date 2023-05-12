e80b041a3d26e982c6315f4c535d90794b9074d0
Emilolund <emil@emilohlund.dev>
2023-05-12 12:21:36 +0200

    🚀 Deploying to Vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://js-api-gql.vercel.app', 'https://studio.apollographql.com/']
+  });
 
   await app.listen(3000);
 }
